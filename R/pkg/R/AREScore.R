7cf968f8da329338b7e39503215258df5fbf33b0
slebedeva <svetlana.lebedeva@mdc-berlin.de>
2021-03-23 18:13:36 +0100

    fixed obsolete Bioc functions in AREscore

--- R/pkg/R/AREScore.R
+++ R/pkg/R/AREScore.R
@@ -1,8 +1,16 @@
+#### source: https://github.com/lianos/seqtools/blob/master/R/pkg/R/AREScore.R
+### thanks Steve! https://lianos.netlify.app/ ## check out his multiGSEA too
+
 ## An implementation of the algorithm to score ARE's as described here:
 ## http://arescore.dkfz.de/info.html
 ## http://www.plosgenetics.org/article/info%3Adoi%2F10.1371%2Fjournal.pgen.1002433
 
-## basal=1;overlapping=1.5;d1.3=.75;d4.6=0.4;d7.9=0.2;within.AU=0.3; aub.min.length=20; aub.p.to.start=0.8; aub.p.to.end=0.55
+## Input is a DNA or RNAString(Set)
+
+## OK it depends on more stuff from his pckage, try to just install it
+#devtools::install_github("lianos/seqtools/R/pkg/")
+
+#basal=1;overlapping=1.5;d1.3=.75;d4.6=0.4;d7.9=0.2;within.AU=0.3; aub.min.length=20; aub.p.to.start=0.8; aub.p.to.end=0.55
 AREscore <- function(x, basal=1.0, overlapping=1.5, d1.3=0.75, d4.6=0.4,
                      d7.9=0.2, within.AU=0.3,
                      aub.min.length=20, aub.p.to.start=0.8, aub.p.to.end=0.55) {
@@ -12,7 +20,7 @@ AREscore <- function(x, basal=1.0, overlapping=1.5, d1.3=0.75, d4.6=0.4,
     overmer <- "ATTTATTTA"
   } else {
     pentamer <- "AUUUA"
-    overmer <- "ATTTATTTA"
+    overmer <- "AUUUAUUUA"
   }
 
   x <- as(x, sprintf("%sStringSet", xtype))
@@ -20,8 +28,8 @@ AREscore <- function(x, basal=1.0, overlapping=1.5, d1.3=0.75, d4.6=0.4,
   pmatches <- vmatchPattern(pentamer, x)
   omatches <- vmatchPattern(overmer, x)
 
-  basal.score <- elementLengths(pmatches) * basal
-  over.score <- elementLengths(omatches) * overlapping
+  basal.score <- elementNROWS(pmatches) * basal
+  over.score <- elementNROWS(omatches) * overlapping
 
   no.cluster <- data.frame(d1.3=0, d4.6=0, d7.9=0)
   clust <- lapply(pmatches, function(m) {
@@ -40,9 +48,9 @@ AREscore <- function(x, basal=1.0, overlapping=1.5, d1.3=0.75, d4.6=0.4,
 
   score <- basal.score + over.score + dscores + aub.score
 
-  ans <- DataFrame(score=score, n.pentamer=elementLengths(pmatches),
-                   n.overmer=elementLengths(omatches), au.blocks=au.blocks,
-                   n.au.blocks=elementLengths(au.blocks))
+  ans <- DataFrame(score=score, n.pentamer=elementNROWS(pmatches),
+                   n.overmer=elementNROWS(omatches), au.blocks=au.blocks,
+                   n.au.blocks=elementNROWS(au.blocks))
   cbind(ans, DataFrame(clust))
 }
 
@@ -60,6 +68,7 @@ AREscore <- function(x, basal=1.0, overlapping=1.5, d1.3=0.75, d4.6=0.4,
 ##' smaller than \code{p.to.end} (their implementation looks like its only
 ##' smaller than!). The last nucleotide of this window is marked
 ##' as the last nucleotide of the AU-block.
+#min.length=20;p.to.start=0.8; p.to.end=0.55
 identifyAUBlocks <- function(x, min.length=20, p.to.start=0.8, p.to.end=0.55) {
   xtype <- match.arg(substr(class(x), 1, 3), c("DNA", "RNA"))
   stopifnot(isSingleNumber(min.length) && min.length >= 5 && min.length <= 50)
@@ -75,13 +84,14 @@ identifyAUBlocks <- function(x, min.length=20, p.to.start=0.8, p.to.end=0.55) {
   }
 
   x <- as(x, sprintf("%sStringSet", xtype))
-
-  au.freq <- letterFrequencyInSlidingView(x, min.length, AU, as.prob=TRUE)
+## apparently it cannot be applied to a stringset?
+  au.freq <- lapply(x, function(y) letterFrequencyInSlidingView(y, min.length, AU, as.prob=TRUE))
   widths <- width(x)
 
   au.blocks <- lapply(1:length(x), function(i) {
+    #print(names(x)[i])
     au <- au.freq[[i]]
-    if (is.null(au) || nrow(au) == 0) {
+    if (is.null(au)) {
       return(IRanges())
     }
     au <- as.numeric(au)
@@ -92,6 +102,11 @@ identifyAUBlocks <- function(x, min.length=20, p.to.start=0.8, p.to.end=0.55) {
     ## posts <- .au.blocks(au, p.to.start, p.to.end)
     posts <- .Call("find_au_start_end", au, p.to.start, p.to.end,
                    PACKAGE="SeqTools")
+    ## make sure to avoid empty blocks, not sure what this function above does
+    posts <- posts[posts$start>0 & posts$end>0,]
+    if (nrow(posts)<1) {
+      return(IRanges())
+    }
     blocks <- IRanges(posts$start, posts$end + min.length - 1L)
     end(blocks) <- ifelse(end(blocks) > widths[i], widths[i], end(blocks))
     reduce(blocks)
